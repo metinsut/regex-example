e791667b0e36b6e9a19c5ed3bfc1afdec885c292
metinsut <metindir@gmail.com>
2018-12-30 19:00:09 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	regex = /^\d{11}$/;
+	regex1 = /\d/;
+	regex2 = /^[\w@-]{3,6}$/;
+	regex3 = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+	state = {
+		val: '',
+		err: false
+	};
+
+	// handleRegex = e => {
+	// 	if (this.regex1.test(e.key)) {
+	// 		console.log('1');
+	// 		return true;
+	// 	} else {
+	// 		console.log('2');
+	// 		e.preventDefault();
+	// 	}
+	// };
+
+	handleState = e => {
+		if (this.regex3.test(e.target.value)) {
+			this.setState({ err: true });
+			this.setState({ val: e.target.value });
+		} else {
+			this.setState({ val: e.target.value });
+			this.setState({ err: false });
+		}
+	};
+
+	render() {
+		return (
+			<div className="app">
+				<input autoFocus type="text" onKeyDown={this.handleRegex} onChange={this.handleState} />
+				<h1 style={this.state.err ? { color: 'green' } : { color: 'red' }}>{this.state.val}</h1>
+				<p>{this.state.err ? 'True' : 'False'}</p>
+			</div>
+		);
+	}
 }
 
 export default App;
